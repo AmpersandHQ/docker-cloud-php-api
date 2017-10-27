b7e185256752e333bcf9cd1673b474a72f6733ec
Tom Bayley <tb@amp.co>
2017-10-27 12:16:49 +0100

    Add setOrganisationNamespace

--- src/API/AbstractInfrastructrueAPI.php
+++ src/API/AbstractInfrastructrueAPI.php
@@ -9,7 +9,15 @@ use DockerCloud\Model\Response\AbstractGetResponse;
 
 abstract class AbstractInfrastructrueAPI extends AbstractAPI
 {
-    protected $api_prifix = '/api/infra/v1';
+    const API_PRIFIX_BASE = '/api/infra/v1';
+    protected $api_prifix = self::API_PRIFIX_BASE;
+
+    /**
+     * @param $namespace
+     */
+    function setOrganisationNamespace($namespace){
+        $this->api_prifix = self::API_PRIFIX_BASE."/".$namespace;
+    }
 
     /**
      * @param $uri
