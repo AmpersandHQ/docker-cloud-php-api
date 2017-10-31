3588979764eab6370b0bd1fa2619cb5314bb9270
Tom Bayley <tb@amp.co>
2017-10-31 11:42:09 +0000

    Change setOrganisationNamespace to public

--- src/API/AbstractInfrastructrueAPI.php
+++ src/API/AbstractInfrastructrueAPI.php
@@ -15,7 +15,7 @@ abstract class AbstractInfrastructrueAPI extends AbstractAPI
     /**
      * @param $namespace
      */
-    function setOrganisationNamespace($namespace){
+    public function setOrganisationNamespace($namespace){
         $this->api_prifix = self::API_PRIFIX_BASE."/".$namespace;
     }
 
