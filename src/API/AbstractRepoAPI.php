3879946e1fc07f7606dbb394b928ccf991c833f8
Tom Bayley <tb@amp.co>
2017-10-31 11:57:06 +0000

    Remove setOrganisationNamespace

--- src/API/AbstractRepoAPI.php
+++ src/API/AbstractRepoAPI.php
@@ -6,13 +6,5 @@ namespace DockerCloud\API;
 
 abstract class AbstractRepoAPI extends AbstractAPI
 {
-    const API_PRIFIX_BASE = '/api/repo/v1';
-    protected $api_prifix = self::API_PRIFIX_BASE;
-
-    /**
-     * @param $namespace
-     */
-    public function setOrganisationNamespace($namespace){
-        $this->api_prifix = self::API_PRIFIX_BASE."/".$namespace;
-    }
+    protected $api_prifix = '/api/repo/v1';
 }
